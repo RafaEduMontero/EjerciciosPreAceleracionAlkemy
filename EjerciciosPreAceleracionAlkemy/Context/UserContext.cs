c3c79e352a4a63304416f3945afa786b15338b1b
Rafael Montero <rafaedumontero@gmail.com>
2021-08-30 19:52:30 -0300

    Agregar archivos de proyecto.

---
+++ EjerciciosPreAceleracionAlkemy/Context/UserContext.cs
@@ -0,0 +1,24 @@
+﻿using EjerciciosPreAceleracionAlkemy.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjerciciosPreAceleracionAlkemy.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
